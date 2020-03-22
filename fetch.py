7a79e52367cc19c5c83fc97cdd544d43ffb490be
Robert <robikoser@gmail.com>
2020-03-22 19:27:35 +0100

    Add site to db and return its id

--- fetch.py
+++ fetch.py
@@ -1,3 +1,4 @@
+import urllib.robotparser
 from urllib.request import urlparse, urljoin
 import requests
 import psycopg2
@@ -14,15 +15,56 @@ from urllib3 import request
 # cur.close()
 # conn.close()
 
-def search_page(url):
+def add_site_to_db(base_url, robots_content, sitemap_content):
+    cur = conn.cursor()
+
+    sql = "SELECT id FROM crawldb.site where domain = %s"
+    cur.execute(sql, (base_url,))
+    record_exists = cur.fetchone()
+
+    #add site if not yet exists
+    if not record_exists:
+        cur.execute("INSERT INTO crawldb.site VALUES(DEFAULT, %s, %s, %s) RETURNING id",
+                    (base_url, robots_content, sitemap_content))
+        id = cur.fetchone()[0]
+    else:
+        id = record_exists[0]
+
+    cur.close()
+
+    return id
+
+
+def check_robots(url):
+    base_url_parser = urlparse(url)
+
+    base_url = base_url_parser.scheme + "://" + base_url_parser.netloc
+
+    rp = urllib.robotparser.RobotFileParser()
+
+    robots_url = urljoin(base_url, '/robots.txt')
+    rp.set_url(robots_url)
+    rp.read()
+
+    # print(rp.site_maps())
+    if rp is None:
+        add_site_to_db(base_url, "", "")
+    else:
+        #TODO rp.site_maps() -> only available in pyhton 3.8, unable to install psycopg2 on pyhton 3.8 with windows
+        soup = BeautifulSoup(requests.get(robots_url).content, 'html.parser')
+        print(str(soup))
+        add_site_to_db(base_url, str(soup), "")
+
+
+def search_page_urls_and_images(url):
     soup = BeautifulSoup(requests.get(url).content, 'html.parser')
     urls = set()
 
-    #get all links
+    # get all links
     for link in soup.findAll('a'):
         parsing_link = link.get('href')
 
-        if (parsing_link == "" or parsing_link is None):
+        if parsing_link == "" or parsing_link is None:
             continue
 
         combined_link = urljoin(url, parsing_link)
@@ -33,12 +75,13 @@ def search_page(url):
             clean_link = parsed_href.scheme + "://" + parsed_href.netloc + parsed_href.path
             urls.add(combined_link) if combined_link not in urls else urls
 
-    #get all images
+    cur = conn.cursor()
+    # get all images
     for image in soup.findAll('img'):
 
         image_source = image.get('src')
 
-        if (image_source == "" or image_source is None):
+        if image_source == "" or image_source is None:
             continue
 
         combined_link = urljoin(url, image_source)
@@ -49,25 +92,25 @@ def search_page(url):
         filename = clean_url.split('/')[-1].split('.')[0]
         file_extension = clean_url.split('/')[-1].split('.')[1]
 
+        cur.execute("INSERT INTO crawldb.image VALUES(DEFAULT, %s, %s, %s, %s, CURRENT_TIMESTAMP)",
+                    (1, filename, file_extension, "BINARY"))
 
-
+        cur.close()
 
     return urls
 
 
-def open_db_connection(host, db, user, password):
-    global conn
-    # connect to the db
-    conn = psycopg2.connect(
-        host='localhost',
-        database='crawler',
-        user='postgres',
-        password='admin'
-    )
+global conn
+# connect to the db
+conn = psycopg2.connect(
+    host='localhost',
+    database='crawler',
+    user='postgres',
+    password='admin'
+)
 
-    conn.autocommit = True
+conn.autocommit = True
 
-
-urls = search_page("https://www.24ur.com/")
-# for links in urls:
-#     print(links)
+# urls = search_page_urls_and_images("https://www.24ur.com/")
+#TODO -> dobimo ID, shrani stran v tabelo page
+check_robots("https://www.gov.si/")
