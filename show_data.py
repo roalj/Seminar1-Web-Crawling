fff9633408fa02dd0c92ba61a341724dd8b9e7b5
pastafariangit <aljosa.omejc@gmail.com>
2020-04-03 22:39:23 +0200

    added limit for number of subsites

--- show_data.py
+++ show_data.py
@@ -21,9 +21,12 @@ def calculate_number_of_pages(sites):
     for s in sites:
         cur.execute(sql, (s[0],))
         num_subpages = cur.fetchone()
-        site_counted.append((num_subpages[0], s[1]))
+        if num_subpages[0] > 10:
+            site_counted.append((num_subpages[0], s[1]))
 
     return site_counted
+
+
 
 
 y = []
@@ -35,9 +38,10 @@ num_rows = 8
 sites = get_all_sites()
 site_counted = calculate_number_of_pages(sites)
 
+
 for s in range(0, len(site_counted), num_rows):
     for j in range(num_rows):
-        if s+j < len(sites):
+        if s+j < len(site_counted):
             y.append(j)
             z.append(s+num_rows)
             a = site_counted[s+j][0]
@@ -52,8 +56,8 @@ ax.axes.get_yaxis().set_visible(False)
 fig.patch.set_visible(False)
 ax.axis('off')
 
-for i in range(0, len(sites), 1):
-    ax.annotate(sites[i][1], (z[i], y[i]), (z[i]+0.2, y[i]))
+for i in range(0, len(site_counted), 1):
+    ax.annotate(site_counted[i][1], (z[i], y[i]), (z[i]+0.2, y[i]))
 
 cur.close()
 plt.show()
