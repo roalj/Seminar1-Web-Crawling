e6aa53fcb3ab8fe97cb190b1e9796d6fedc8cd2f
Robert <robikoser@gmail.com>
2020-04-03 22:51:45 +0200

    Change font size, draw url once on top once on bot side (overlaying)

--- show_data.py
+++ show_data.py
@@ -1,10 +1,11 @@
 import matplotlib.pyplot as plt
 import psycopg2
+import math
 
 conn = psycopg2.connect(
     host='localhost',
-    database='crawldb',
-    user='crawldb',
+    database='test',
+    user='postgres',
     password='admin'
 )
 conn.autocommit = True
@@ -21,7 +22,7 @@ def calculate_number_of_pages(sites):
     for s in sites:
         cur.execute(sql, (s[0],))
         num_subpages = cur.fetchone()
-        if num_subpages[0] > 10:
+        if num_subpages[0] > 50:
             site_counted.append((num_subpages[0], s[1]))
 
     return site_counted
@@ -33,7 +34,7 @@ y = []
 z = []
 size = []
 fig_dims = (25, 10)
-num_rows = 8
+num_rows = 5
 
 sites = get_all_sites()
 site_counted = calculate_number_of_pages(sites)
@@ -46,9 +47,12 @@ for s in range(0, len(site_counted), num_rows):
             z.append(s+num_rows)
             a = site_counted[s+j][0]
             size.append(50 + a)
-            print(s+num_rows)
 
+font = {'family' : 'normal',
+        'weight' : 'bold',
+        'size'   : 16}
 
+plt.rc('font', **font)
 fig, ax = plt.subplots(figsize=fig_dims)
 ax.scatter(z, y, size)
 ax.axes.get_xaxis().set_visible(False)
@@ -57,7 +61,8 @@ fig.patch.set_visible(False)
 ax.axis('off')
 
 for i in range(0, len(site_counted), 1):
-    ax.annotate(site_counted[i][1], (z[i], y[i]), (z[i]+0.2, y[i]))
+    print(math.pow(-1,i))
+    ax.annotate(site_counted[i][1], (z[i], y[i]), (z[i], y[i] + (0.2 *math.pow(-1,i))))
 
 cur.close()
 plt.show()
